240ec2b21ed153dab8bdacc4b18595dde4f218e1
Hugo <Hugo@gmail.com>
2025-07-30 13:41:24 +0200

    Terminados testGetAll y testUpdateArbol

--- src/test/java/es/cic25/proy009/controller/ArbolControllerIntegrationTest.java
+++ src/test/java/es/cic25/proy009/controller/ArbolControllerIntegrationTest.java
@@ -1,12 +1,15 @@
 package es.cic25.proy009.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import es.cic25.proy009.model.Arbol;
@@ -118,20 +122,89 @@ public class ArbolControllerIntegrationTest {
 
         Long idGenerado = arbolRepository.save(arbol).getId();
 
-        //borramos el arbol con el id que se ha generado
+        // Borramos el arbol con el id que se ha generado
         mockMvc.perform(delete("/arbol/" + idGenerado))
         .andDo(print())
         .andExpect(status().isOk());
 
-        //comprobamos tambien que no existe
+        // Comprobamos que no existe el arbol
         assertTrue(arbolRepository.findById(idGenerado).isEmpty());
-        //comprobamos si se han borrado las ramas
+
+        // Comprobamos si se han borrado las ramas
         assertTrue(ramaRepository.findAll().size() == 0);
 
     }
 
     @Test
     void testGetAll() throws Exception {
+
+        // Creamos los arboles y les asignamos sus ramas
+
+        // Arbol 1
+        Arbol arbol1 = new Arbol();
+        arbol1.setColor("Verde");
+        arbol1.setEdad(57);
+        arbol1.setEspecie("Manzano");
+        arbol1.setFruto("Manzana");
+        arbol1.setPerenne(false);
+        arbol1.setPeso(50.7);
+
+        Rama rama1 = new Rama();
+        rama1.setArbol(arbol1);
+        rama1.setGrosor(5);
+        rama1.setHojaCompuesta(false);
+        rama1.setLongitud(20);
+        rama1.setNumHojas(7);
+
+        Rama rama2 = new Rama();
+        rama2.setArbol(arbol1);
+        rama2.setGrosor(7);
+        rama2.setHojaCompuesta(false);
+        rama2.setLongitud(15);
+        rama2.setNumHojas(9);
+
+        arbol1.getRamas().add(rama1);
+        arbol1.getRamas().add(rama2);
+
+        // Arbol 2
+        Arbol arbol2 = new Arbol();
+        arbol2.setColor("Rojo");
+        arbol2.setEdad(20);
+        arbol2.setEspecie("Inventado");
+        arbol2.setFruto("Inventado");
+        arbol2.setPerenne(false);
+        arbol2.setPeso(15);
+
+        Rama rama3 = new Rama();
+        rama3.setArbol(arbol2);
+        rama3.setGrosor(8);
+        rama3.setHojaCompuesta(false);
+        rama3.setLongitud(25);
+        rama3.setNumHojas(10);
+
+        Rama rama4 = new Rama();
+        rama4.setArbol(arbol2);
+        rama4.setGrosor(10);
+        rama4.setHojaCompuesta(false);
+        rama4.setLongitud(5);
+        rama4.setNumHojas(3);
+
+        arbol2.getRamas().add(rama3);
+        arbol2.getRamas().add(rama4);
+
+        // Creamos los registros en la base de datos
+        arbolRepository.save(arbol1);
+        arbolRepository.save(arbol2);
+
+        mockMvc.perform(get("/arbol"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(result -> {
+            String response = result.getResponse().getContentAsString();
+            List<Arbol> personas = objectMapper.readValue(response,new TypeReference<List<Arbol>>() {}); 
+
+            assertTrue(personas.size() >= 2);
+        });
 
     }
 
@@ -177,6 +250,49 @@ public class ArbolControllerIntegrationTest {
 
     @Test
     void testUpdateArbol() throws Exception {
+
+        // Creamos un arbol
+        Arbol arbol = new Arbol();
+        arbol.setColor("Verde");
+        arbol.setEdad(57);
+        arbol.setEspecie("Manzano");
+        arbol.setFruto("Manzana");
+        arbol.setPerenne(false);
+        arbol.setPeso(50.7);
+
+        // creamos las ramas y las añadimos a la lista
+        Rama rama1 = new Rama();
+        rama1.setArbol(arbol);
+        rama1.setGrosor(5);
+        rama1.setHojaCompuesta(false);
+        rama1.setLongitud(20);
+        rama1.setNumHojas(7);
+
+        Rama rama2 = new Rama();
+        rama2.setArbol(arbol);
+        rama2.setGrosor(7);
+        rama2.setHojaCompuesta(false);
+        rama2.setLongitud(15);
+        rama2.setNumHojas(9);
+
+        arbol.getRamas().add(rama1);
+        arbol.getRamas().add(rama2);
+
+        Arbol arbolGenerado = arbolRepository.save(arbol);
+
+        // Comprobamos el color
+        assertTrue(arbolGenerado.getColor().equals("Verde"));
+        // Le cambiamos el color
+        arbolGenerado.setColor("Amarillo");
+        // Serializamos el árbol
+        String arbolJson = objectMapper.writeValueAsString(arbolGenerado);
+
+        mockMvc.perform(put("/arbol")
+        .contentType("application/json")
+        .content(arbolJson))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.color").value("Amarillo"));
 
     }
 }
