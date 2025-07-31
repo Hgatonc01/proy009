f0bdeda49e78c6443c0a757e97442b4909e76243
Hugo <Hugo@gmail.com>
2025-07-31 11:31:17 +0200

    Cosas

---
+++ src/test/java/es/cic25/proy009/RamaIntegrationTest.java
@@ -0,0 +1,16 @@
+package es.cic25.proy009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RamaIntegrationTest {
+
+    @Test
+    void testDeleteArbol() throws Exception {
+
+    }
+
+}
